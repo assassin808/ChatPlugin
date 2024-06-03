ad2da8a0977eb7c2a630f202fbbba8641648eb50
商昊暘521021910816 <assassin808@sjtu.edu.cn>
2024-06-03 10:05:53 +0800

    chore: Add gitignore, package.json, chat.css, note.md, app.mjs, and index.html files

---
+++ app.mjs
@@ -0,0 +1,53 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+  
+const app = express();  
+const __filename = fileURLToPath(import.meta.url);  
+const __dirname = path.dirname(__filename);  
+app.use(express.static(path.join(__dirname, 'public')));  
+app.use(express.json());  
+app.use(express.urlencoded({ extended: false }));  
+  
+
+app.get('/', (req, res) => {  
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));  
+});  
+  
+const router = express.Router();  
+
+app.use('/api', router);  
+  
+
+router.post('/generate-text', async (req, res) => {  
+  const prompt = req.body.prompt;  
+  const maxTokens = req.body.maxTokens;  
+  try {  
+    const gptResponse = await axios.post(`${process.env.END_POINT}`, {
+            messages: [
+                { role: 'system', content: 'You are a helpful assistant.' },
+                { role: 'user', content: prompt },
+            ]
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': `${process.env.AZURE_API_KEY}` 
+            }
+        }); 
+    res.json(gptResponse.data);
+  } catch (err) {  
+    // res.json({ message: err.message });  
+    console.log(err);
+  }  
+});  
+
+  
+
+  
+app.listen(3000, () => {  
+  console.log('Server is running on http://localhost:3000');  
+});  
---
+++ note.md
@@ -0,0 +1,12 @@
+- Design barriers: "I don't even know what I want the computer to do..."
+- Selection barriers: "I know what I want the computer to do, but I don't know what to use..."
+- Coordination barriers: "I know what things to use, but I don't know how to make them work together..."
+- Use barriers: "I know what to use, but I don't know how to use it..."
+- Understanding barriers: "I thought I knew how to use this, but it didn't do what I expected..."
+- Information barriers: "I know why it didn't do what I expected, but I don't know how to check..."
+
+
+In this case, we note that rather
+than making the app interface try to act more human, there may
+be useful design opportunities by making the app interface act
+explicitly in non-human ways.
